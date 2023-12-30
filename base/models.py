d37d16e1cff4376427f4dcac6afdf7fb84be299a
chaos <prathamsp79@gmail.com>
2023-12-30 22:23:58 +0530

    just chaging authors to another account ignore

--- base/models.py
+++ base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-# just cahnging authors check aging
+# just cahnging authors check agian
 class Room(models.Model):
     #host =
     #topic =

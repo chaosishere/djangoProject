82527da57c4804bf6929baefb6632bba6f4a5240
chaos <prathamsp79@gmail.com>
2023-12-31 01:02:42 +0530

    created new models for room with Topic, Message and User (with inbuilt django USER class) and inherited its uses with registrations via admin panel and databases

--- base/views.py
+++ base/views.py
@@ -3,20 +3,18 @@ from django.http import HttpResponse
 from .models import Room
 #creating room
 
-rooms = [
-    {'id': 1, 'name':'lets learn python!'},
-    {'id': 2, 'name': 'design me!'},
-    {'id': 3, 'name': 'backend developers'},
-]
+#rooms = [
+    #{'id': 1, 'name':'lets learn python!'},
+   # {'id': 2, 'name': 'design me!'},
+   # {'id': 3, 'name': 'backend developers'},
+#]
 
 def home(request):
     rooms = Room.objects.all() #overwrite the rooms from above to the models and added via admin panel
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+
     return render(request, 'base/room.html',{'room': room})
 
